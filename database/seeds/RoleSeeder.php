5bc0533a3ac664231b47a1ebd500b00e42d5a6db
Vicktor Desrony <vldcreation21@gmail.com>
2021-06-21 16:14:16 +0700

    Update Database

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $data = array('admin','pengurus','pengguna');
+        foreach($data as $role):
+            DB::table('role')->insert([
+                'role_name' => $role,
+                'created_at' => date("Y-m-d h:i:sa", $time())
+            ]);   
+        endforeach;
+    }
+}
