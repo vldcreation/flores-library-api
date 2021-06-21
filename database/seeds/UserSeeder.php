a96a2c6fdfe38932f5bc4090cc80e9b00ec3f102
Vicktor Desrony <vldcreation21@gmail.com>
2021-06-21 12:29:55 +0700

    Create Seeder through DB

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $Cur = DB::table('users')->orderBy('id', 'desc')->first();
+        if(!$Cur)
+            $Cur['id'] = 0;
+        $getId = 'user-'.($Cur['id'] + 1);
+        DB::table('users')->insert([
+            'role' => 2,
+            'name' => "vicktor desrony",
+            'email' => "vicktordesrony@gmail.com",
+            'username' => $getId,
+            'password' => md5('123456'),
+        ]);
+    }
+}
