650ef552dee9300d5b22a01b7eec15c49b6a17e8
Vicktor Desrony <vldcreation21@gmail.com>
2021-06-22 23:24:02 +0700

    Add Review API

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Review;
+use Illuminate\Http\Request;
+use App\Http\Resources\StatusCode;
+
+class ReviewController extends Controller
+{
+    public function getReviews(){
+        $data = Review::all();
+        return response()->json([
+            'data' => $data,
+            'message' => StatusCode::http_response_code(200)
+        ]);
+    }
+
+    public function getReviewById($id){
+        $data = Review::find($id);
+        if($data){
+            return response()->json([
+                'data' => $data,
+                'message' => StatusCode::http_response_code(200)
+            ]);
+        }
+        return response()->json([
+            'message' => StatusCode::http_response_code(404)
+        ]);
+    }
+
+    public function getBookRating($id_buku){
+        $data = Review::where('id_buku',$id_buku)->get();
+        $countRecord = $data->count();
+        $CountRating = $data->sum('rating');
+        return round($CountRating/$countRecord,1);
+        // return $CountRating;
+    }
+
+    public function addReview(Request $request){
+        $data = Review::create($request->all());
+        if($data){
+            return response()->json([
+                'data' => $data,
+                'message' => StatusCode::http_response_code(201)
+            ]);
+        }
+        return response()->json([
+            'message' => StatusCode::http_response_code(406)
+        ]);
+    }
+
+    public function updateReview(Request $request,$id){
+        $data = Review::find($id);
+        if($data){
+            $data->update($request->all());
+            return response()->json([
+                'data' => $data,
+                'message' => StatusCode::http_response_code(201)
+            ]);
+        }
+        return response()->json([
+            'message' => StatusCode::http_response_code(404)
+        ]);
+    }
+
+    public function deleteReview($id){
+        $data = Review::find($id);
+        if($data){
+            if($data->delete()){
+                return response()->json([
+                    'response' => 'Record was Delete',
+                    'message' => StatusCode::http_response_code(202)
+                ]);
+            }
+            //Failde Deleete
+            return response()->json([
+                'message' => StatusCode::http_response_code(406)
+            ]);
+
+        }
+        //Record not found
+        return response()->json([
+            'message' => StatusCode::http_response_code(404)
+        ]);
+    }
+}
