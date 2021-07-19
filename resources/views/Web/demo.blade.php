ef11c09b57f0ae46e8688b5fceaddee01940b839
Vicktor Desrony <vldcreation21@gmail.com>
2021-07-19 15:19:14 +0700

    View for demo

---
+++ resources/views/Web/demo.blade.php
@@ -0,0 +1,47 @@
+<?php 
+use Illuminate\Support\Facades\DB;
+use App\BookCategory;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/demo2.css') }}">
+    <title>Demo Book</title>
+</head>
+<body>
+ <div class="row">
+     <div class="header-book">
+         <h4>Daftar Buku</h4>
+     </div>
+ </div>
+ <div class="root-container">
+<div class="container">
+    @foreach($data as $book)
+  <div class="card">
+    <div class="card-header">
+      <img src=" {{ asset('assets/file-image/'.$book->gambar_buku) }}" alt="rover" />
+    </div>
+    <div class="card-body">
+      <span class="tag tag-teal"><?php echo BookCategory::where('id',$book->id_kategori)->get()[0]->nama_kategori;  ?></span> 
+      <h4>
+        {{ $book->judul }}
+      </h4>
+      <p>
+      {{ $book->deskripsi }}
+      </p>
+      <div class="user">
+        <div class="user-info">
+          <h5>{{$book->penulis}}</h5>
+          <small>2h ago</small>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endforeach
+</div>
+</div>
+</body>
+</html>
---
+++ resources/views/Web/detaildemo.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+    <script src="https://kit.fontawesome.com/76e9768b91.js"></script>
+    <title>Demo Book</title>
+</head>
+<body>
+<div class="container">
+  <div class="info">
+    Mobile - Try tapping
+  </div>
+  <div class="mobile-layout">
+    <div class="notification-header">
+      <div class="time">
+        9:41
+      </div>
+      <div class="necessities">
+        <i class="fas fa-signal"></i>
+        <i class="fas fa-wifi"></i>
+        <i class="fas fa-battery-full"></i>
+      </div>
+    </div>
+    <div class="actions">
+      <i class="fas fa-chevron-left"></i>
+      <i class="fas fa-bookmark"></i>
+    </div>  
+    <div class="book-cover">
+        <img class="book-top" src="https://saranyamk.github.io/images-repo/book-top.svg" alt="book-top" />
+      <img class="book-side" src="https://saranyamk.github.io/images-repo/book-side.svg" alt="book-side" /> 
+    </div>
+    <div class="preface">
+      <div class="content">
+        <div class="header">
+          <div class="title">The Diary of a Young Girl</div>
+          <div class="icon">
+            <i class="fas fa-chevron-down"></i>
+          </div>
+        </div>
+        <div class="author">by Anne Frank</div>
+        <div class="body">
+          <p>
+            also known as The Diary of Anne Frank, is a book of the writings from the Dutch-language diary kept by Anne Frank while she was in hiding for two years with her family during the Nazi occupation of the Netherlands
+          </p>
+          <p>
+            Anne calls her diary "Kitty", so almost all of the letters are written to Kitty. 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--
+https://saranyamk.github.io/images-repo/book-top.svg
+https://saranyamk.github.io/images-repo/book-side.svg
+-->
+</body>
+</html>
